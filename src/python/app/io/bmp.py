ef0c97626e4810a6d8607346919822a318919fe3
Konrad Bodzioch <kbodzio@student.agh.edu.pl>
2025-12-15 18:01:35 +0100

    operations: linter fixes

--- src/python/app/io/bmp.py
+++ src/python/app/io/bmp.py
@@ -77,7 +77,7 @@ class BMPReader(IFormatReader):    # pylint: disable=too-few-public-methods
 
     @override
     def read_format(self, file: BinaryIO) -> Image:
-        header = BitmapFileHeader.from_bytes(data=file.read(BitmapFileHeader.HEADER_LENGTH))
+        # header = BitmapFileHeader.from_bytes(data=file.read(BitmapFileHeader.HEADER_LENGTH))
 
         dib_header_size = file.read(4)
         dib_header_size, = struct.unpack('I', dib_header_size)
